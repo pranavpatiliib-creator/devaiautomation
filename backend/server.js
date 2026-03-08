7c6c26344cf298f3dd6ae1a121fdcbd950838c88
Pranav Patil <pranavpatiliib@gmail.com>
2026-03-08 14:48:13 +0530

    Fix package.json

--- backend/server.js
+++ backend/server.js
@@ -249,9 +249,8 @@ app.post("/lead-public", (req, res) => {
 
 
 // ================= SERVER START =================
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-
-    console.log("🚀  Backend Server running on http://localhost:5000");
-
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
