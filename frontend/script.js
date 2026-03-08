0898ff5562435d881054ae5309c542f013ee88d9
Pranav Patil <pranavpatiliib@gmail.com>
2026-03-08 15:51:40 +0530

    Fix Render API URL

--- frontend/script.js
+++ frontend/script.js
@@ -3,7 +3,7 @@ const API_BASE_URL =
     (window.location.hostname === "localhost" ||
         window.location.hostname === "127.0.0.1")
         ? "http://localhost:5000"
-        : "https://devaiautomation.onrender.comm";
+        : "https://devaiautomation.onrender.com";
 
 
 
