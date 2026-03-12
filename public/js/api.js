9b9d7351785eebdc853637528c7da2f3fb6917a8
Pranav Patil <pranavpatiliib@gmail.com>
2026-03-12 22:33:26 +0530

    vercel issue

--- public/js/api.js
+++ public/js/api.js
@@ -4,7 +4,7 @@ const API_BASE_URL =
     (window.location.hostname === "localhost" ||
         window.location.hostname === "127.0.0.1")
         ? "http://localhost:5000"
-        : "https://devaiautomation.onrender.com";
+        : "";
 
 // ================= API CALLS =================
 
